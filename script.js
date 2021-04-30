77863d430a5abbf47a62310189fed2e116b7e6b9
Gutemberg Medeiros <gutenbergdevm@hotmail.com>
2021-04-30 15:33:17 -0300

    Terminada a lógica das parcelas

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 
-
 function menuOn() {
     let toggle = document.querySelector('#toggle');
 
@@ -21,11 +20,9 @@ function menuOff() {
 }
 parcelasX.forEach(parcelas => {parcelas.addEventListener("click", menuOff)});
 
-
-
-
-
-function contadorParcelas() {
+function contadorParcelas(event) {
+    let parcelaNun = event.target.textContent;
+    console.log(parcelaNun);
     let inValor = document.querySelector('#inValor');
 
     let valor = Number(inValor.value);
@@ -36,31 +33,31 @@ function contadorParcelas() {
         return;
     }
 
-    if(parcelasX[0]) {
+    if(parcelaNun == '1X') {
         let total = valor + (valor * 0.1) / 1;
 
         alteraCont[0].textContent = `R$${total.toFixed(2)}`;
         alteraCont[1].textContent = `1X`;
         alteraCont[2].textContent = `R$${total.toFixed(2)}`;
-    } else if(parcelasX[1]) {
+    } else if(parcelaNun == '2X') {
         let total2 = valor + (valor * 0.2);
 
         alteraCont[0].textContent = `R$${total2.toFixed(2)}`;
         alteraCont[1].textContent = `2X`;
         alteraCont[2].textContent = `R$${total2.toFixed(2) / 2}`;
-    } else if(parcelasX[2]) {
+    } else if(parcelaNun == '3X') {
         let total3 = valor + (valor * 0.3);
 
         alteraCont[0].textContent = `R$${total3.toFixed(2)}`;
         alteraCont[1].textContent = `3X`;
         alteraCont[2].textContent = `R$${ (total3.toFixed(2) / 3).toFixed(2)}`;
-    } else if(parcelasX[3]) {
+    } else if(parcelaNun == '4X') {
         let total4 = valor + (valor * 0.4);
 
         alteraCont[0].textContent = `R$${total4.toFixed(2)}`;
         alteraCont[1].textContent = `4X`;
         alteraCont[2].textContent = `R$${total4.toFixed(2) / 4}`;
-    } else if(parcelasX[4]){
+    } else if(parcelaNun == '5X'){
         let total5 = valor + (valor * 0.5);
 
         alteraCont[0].textContent = `R$${total5.toFixed(2)}`;
@@ -68,8 +65,15 @@ function contadorParcelas() {
         alteraCont[2].textContent = `R$${total5.toFixed(2) / 5}`;
     }
 }
-parcelasX[length].addEventListener("click", contadorParcelas);
+parcelasX.forEach(contador => {contador.addEventListener("click", contadorParcelas)});
 
 
 
+
+/*
+function somar(a, b) {
+    return a + b;
+}
+
+*/
 
